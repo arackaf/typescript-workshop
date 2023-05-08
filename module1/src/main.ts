a62b2bc44ac9d0589a2937fd1dd59a7c6a1ab535
Adam Rackis <adamrackis@hotmail.com>
2023-05-08 18:27:15 -0500

    Cleanup

--- module1/src/main.ts
+++ module1/src/main.ts
@@ -1,5 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.ts";
 import "./sandbox";
@@ -9,9 +8,6 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
