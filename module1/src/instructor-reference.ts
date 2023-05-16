0073bc3126a4521da309469e99bda8bead5be9ad
Adam Rackis <adamrackis@hotmail.com>
2023-05-15 23:07:02 -0500

    More setup

--- module1/src/instructor-reference.ts
+++ module1/src/instructor-reference.ts
@@ -5,9 +5,15 @@ let s = "Hello";
 //s = 12;
 
 let s2: string;
-
 // error
 //s2 = 12;
+
+// types
+let b: boolean;
+let n: number;
+
+let u: unknown;
+let a: any;
 
 function getNumber(): number {
   return 12;
@@ -37,6 +43,43 @@ function logAll2(...values: (string | number)[]) {
 
 logAll2(1, "2", 3, "4", 5, "6");
 
-// TODO: any, unknown, never
+type TwoStringsReturnNumber = (s1: string, s2: string) => number;
 
-// Tuple types, indexed types
+const fn: TwoStringsReturnNumber = (s1, s2) => {
+  return s1.length + s2.length;
+};
+
+type BoolForSomeReason = boolean;
+
+// types can be combined
+
+type BoolOrString = boolean | string;
+// union - we'll just pretend it means or ... more on this later
+
+let bool: BoolForSomeReason = true;
+
+type SomeObject = {
+  name: string;
+  age: number;
+  active: boolean;
+};
+// grab a slice of a type
+
+// type SomeObjectName = SomeObject.name;
+// type SomeObjectName = SomeObject::name;
+type SomeObjectName = SomeObject["name"];
+
+type KeyOfSomeObject = keyof SomeObject;
+let x: KeyOfSomeObject;
+
+// now let's peak ahead - generics come later, but let's see something neat ...
+
+type TypeOfSomeObjectProperty<T extends keyof SomeObject> = SomeObject[T];
+type Name = TypeOfSomeObjectProperty<"name">;
+
+// Tuples
+type StringNumberTuple = [string, number];
+
+let tuple: StringNumberTuple = ["", 2];
+
+let tupleStr = tuple[0];
