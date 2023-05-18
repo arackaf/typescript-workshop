a0a351b6acba47d62ad4f701ee56b581076c0ce9
Adam Rackis <adamrackis@hotmail.com>
2023-05-17 22:16:24 -0500

    Tweaks

--- module5-react/src/Component1.tsx
+++ module5-react/src/Component1.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 
 export const Component1: FC<any> = (props) => {
   return (
--- module5-react/src/Component2.tsx
+++ module5-react/src/Component2.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 
 export const Component2: FC<any> = (props) => {
   return (
