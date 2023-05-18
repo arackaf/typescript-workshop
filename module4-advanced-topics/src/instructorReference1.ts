5a4d76b85a7486f464a759dbc440e601ebc83862
Adam Rackis <adamrackis@hotmail.com>
2023-05-17 21:42:30 -0500

    tweak

--- module4-advanced-topics/src/instructorReference1.ts
+++ module4-advanced-topics/src/instructorReference1.ts
@@ -34,6 +34,8 @@ getShape = getCircle;
 
 // Conditional types
 
+type Useless = Array<number> extends object ? true : false;
+
 type IsArray<T> = T extends Array<unknown> ? true : false;
 
 type A = IsArray<string[]>;
