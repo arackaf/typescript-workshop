169ed9a33ea6dee0a618e4991500bc39d466fa77
Adam Rackis <adamrackis@hotmail.com>
2023-05-28 18:58:17 -0500

    tweaks

--- module3-unions/src/instructorReference.ts
+++ module3-unions/src/instructorReference.ts
@@ -30,3 +30,30 @@ export function dealWithCatOrDog2(animal: Cat | Dog) {
     console.log("Dog", animal.name, animal.dogBreed, animal.obedient);
   }
 }
+
+type StringOrNumber = string | number;
+
+let sOrN: StringOrNumber = Math.random() < 0.5 ? 12 : "Hello";
+
+if (typeof sOrN === "string") {
+  console.log(sOrN.toLowerCase());
+} else {
+  console.log(sOrN.toFixed(2));
+}
+
+class A {
+  a: string = "A";
+}
+
+class B {
+  b: string = "B";
+}
+
+type AorB = A | B;
+let aOrB: AorB = Math.random() < 0.5 ? new A() : new B();
+
+if (aOrB instanceof A) {
+  console.log(aOrB.a);
+} else {
+  console.log(aOrB.b);
+}
