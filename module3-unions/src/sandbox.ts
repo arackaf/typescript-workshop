c0cb5c5ab1ec5c3e02d37c3a26ccfee972b0cfd9
Adam Rackis <adamrackis@hotmail.com>
2023-10-22 18:29:23 -0500

    tweak

--- module3-unions/src/sandbox.ts
+++ module3-unions/src/sandbox.ts
@@ -1,11 +1,13 @@
 type Cat = {
   name: string;
   whiskers: number;
+  felineBreed: string;
 };
 
 type Dog = {
   name: string;
   obedient: boolean;
+  canineBreed: string;
 };
 
 export default null;
